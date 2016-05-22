b242c1241b0bf796dd58357d04a04f0092cc4d33
ssbbnw <ssbbnw@gmail.com>
2016-05-21 21:49:06 -0500

    Create GameBoard.java

---
+++ GameBoard.java
@@ -0,0 +1,8 @@
+
+public class GameBoard {
+	public int gameBoard [][][] = new int[4][4][4];
+	public static void main(String[] args) {
+		Graphics e = new Graphics();
+		System.out.println(e.toString());
+	}
+}
