b11e13d794caa859a072f8ea4fe2cd267ab6fe50
ssbbnw <ssbbnw@gmail.com>
2016-05-23 10:32:21 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,48 @@
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Main {
-
+public class Main extends JFrame implements ActionListener {
+	
+	private JButton resetButton;
+	private Container window = getContentPane();
+	private Graphics g;
+	
 	public static void main(String[] args) {
+		Main frame = new Main();
 		
-		// Create the graphics
-		Graphics g = new Graphics();
+		frame.createMenu();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setTitle("4-D Tic-Tac-Toe");
+	}
+	
+	private void createMenu() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		setResizable(false);
 		
-		// Set the features of the window
-		JFrame frm = new JFrame("4D Tic-Tac-Toe"); 
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm.getContentPane().setLayout(new BorderLayout());
-		frm.getContentPane().add(new Graphics(), BorderLayout.CENTER);
-		frm.pack();
-		frm.setLocationRelativeTo(null);
-		frm.setResizable(false);
-		frm.setVisible(true);
+		window.setLayout(new BorderLayout());
 		
+		resetButton = new JButton("Reset");
+		resetButton.addActionListener(this);
+		
+		window.add(resetButton, BorderLayout.WEST);
+		g = new Graphics();
+		window.add(g, BorderLayout.EAST);
+		
+		pack();
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		window.remove(g);
+		g = new Graphics();
+		window.add(g, BorderLayout.EAST);
+		pack();
+	}
+	
 }
