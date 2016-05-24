7108fe3cabadbf350b8493b2785a9ca7d87ce438
ssbbnw <ssbbnw@gmail.com>
2016-05-24 10:33:48 -0500

    Update Board2D.java

--- Board2D.java
+++ Board2D.java
@@ -1,4 +1,5 @@
 
+// Import required libraries
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
@@ -6,11 +7,13 @@ import javax.swing.JPanel;
 
 public class Board2D extends JPanel {
 	
+	// Set static variables for row and column size
 	public static final int ROWS = 4;
 	public static final int COLUMNS = 4;
 	
 	public BoardButton[][] buttonArray = new BoardButton[4][4];
 	
+	// Create 2D array of buttons
 	public Board2D(int plane, ActionListener al) {
 		setLayout(new GridLayout(ROWS, COLUMNS));
 		
